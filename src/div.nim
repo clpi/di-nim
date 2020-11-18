8afdb1e358fa51e1a5fcb536aade12707fd696bf
Chris P <chrisp1877@gmail.com>
2020-11-18 05:21:20 -0800

    adding more cli handling

--- src/div.nim
+++ src/div.nim
@@ -1,5 +1,10 @@
 import os, parseopt, math, Terminal,  tables, logging
 import strutils, parseutils, sequtils, re
+import
+  models/item,
+  models/record,
+  models/field,
+  lexer
 
 if logging.getHandlers().len == 0:
   var logger = logging.newConsoleLogger()
@@ -8,8 +13,16 @@ if logging.getHandlers().len == 0:
 var
   verbose, pretty, deubug: bool = false
   tokens: seq[string] = @[]
-  item, record, field: string = ""
+  citem, crecord, cfield: string = ""
   tag: string = ""
+
+type
+  FieldKind = enum
+    int, str, date
+  Status = enum
+    active, inactive
+
+var itemList, recordList, fieldList: array[0, string]
 
 proc initData =
   case existsOrCreateDir("~/.config/di"):
@@ -19,23 +32,46 @@ proc initData =
     echo "Dir exists"
 
 proc readItems =
-  echo "Reading items..."
-
-proc readFields =
+  setForegroundColor(fgMagenta)
   echo "Reading items..."
 
 proc readRecords =
-  echo "Reading items..."
+  setForegroundColor(fgBlue)
+  echo "Reading records..."
+
+proc readFields =
+  setForegroundColor(fgGreen)
+  echo "Reading fields..."
+
+proc createItem =
+  setForegroundColor(fgMagenta)
+  echo "Creating item: ", paramStr(3)
+
+proc createRecord =
+  setForegroundColor(fgBlue)
+  echo "Creating record: ", paramStr(3)
+
+proc createField =
+  setForegroundColor(fgGreen)
+  echo "Creating field: ", paramStr(3)
 
 proc listItem() =
   setForegroundColor(fgMagenta)
   echo "Items:"
+  for item in itemList:
+    echo item
+
 proc listRecord() =
   setForegroundColor(fgBlue)
   echo "Records:"
+  for record in recordList:
+    echo record
+
 proc listField() =
   setForegroundColor(fgGreen)
   echo "Fields:"
+  for field in fieldList:
+    echo field
 
 proc processItem() =
   var name: string = paramStr(2)
@@ -54,14 +90,14 @@ proc processField() =
 
 proc process()  =
   setForegroundColor(fgYellow)
-  echo("Processing...")
   var config: string = ""
   var cmdArgs = newSeq[string](0)
   let version = "div 0.0.1"
   let info = """
   Usage:
-    div [record|item|field] [name] [value] [tag] [options]
+    div [options] <record|item|field> [name] [value] [tag] [params]
 
+  Subcomands:
   Arguments:
     [record|item|field]  The object to assign a value to
     [value]              The value assigned to the object
@@ -71,6 +107,8 @@ proc process()  =
     -v, --version        Prints the version of div
     -c, --config         Opens configuration in default editor
     -l, --list           List all objects of all types with values
+  Parameters:
+
   """
   for kind, key, val in getopt():
     case kind:
@@ -81,20 +119,36 @@ proc process()  =
         echo "No params provided"
       of 1:
         case paramStr(1):
-        of "item":
-          listItem()
-        of "record":
-          listRecord()
-        of "field":
-          listField()
+        of "item": listItem()
+        of "record": listRecord()
+        of "field": listField()
       of 2:
         case paramStr(1):
         of "item":
           processItem()
+          break
         of "record":
           processRecord()
+          break
         of "field":
           processField()
+          break
+        of "new":
+          break
+      of 3:
+        case paramStr(1):
+        of "new":
+          case paramStr(2):
+          of "item":
+            createItem()
+            break
+          of "record":
+            createRecord()
+            break
+          of "field":
+            createField()
+            break
+        else: discard
       else:
         cmdArgs.add key
         echo info
